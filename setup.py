60023a6e565cff61e805fdf39285b3cf1f6af7a2
Keagan-EXPLORE <keagan@explore-ai.net>
2021-12-10 14:14:18 +0200

    package `xbot` for distribution

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xbot',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='xbot - The EXPLORE Command Line Interface tool for interacting with your data mesh platform',
+    long_description=open('README.md').read(),
+    install_requires=['requests','argparse','datetime','pytz'],
+    url='https://github.com/Explore-AI/EXPLORE.Utilities.CORE.xbot',
+    author='EXPLORE-AI CORE Developers',
+    author_email='keagan@explore-ai.net'
+)
