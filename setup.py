523f33a45d19ba97ab2711bc2979a20f9b1be1f3
Keagan-EXPLORE <keagan@explore-ai.net>
2021-12-10 14:36:08 +0200

    [installation]: update package name to be more explicit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='xbot',
+    name='xbot-cli',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
