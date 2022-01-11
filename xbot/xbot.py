9da7f8e8bf2dba88d06bb057ba5b20d5e9b622f1
Keagan-EXPLORE <keagan@explore-ai.net>
2022-01-11 11:22:23 +0200

    [refactor]: add double dash and remove shorthand flags

--- xbot/xbot.py
+++ xbot/xbot.py
@@ -12,148 +12,139 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument(
-    "-l", "-login", help="provide credentials to login to the mesh", action="store_true"
+    "-login", help="provide credentials to login to the mesh", action="store_true"
 )
 parser.add_argument(
-    "-all_nodes", help="list all nodes in the mesh", action="store_true"
+    "--all_nodes", help="list all nodes in the mesh", action="store_true"
 )
 parser.add_argument(
-    "-all_ports", help="list all ports in the mesh", action="store_true"
+    "--all_ports", help="list all ports in the mesh", action="store_true"
 )
 parser.add_argument(
-    "-total_nodes", help="list total number of nodes in the mesh", action="store_true"
+    "--total_nodes", help="list total number of nodes in the mesh", action="store_true"
 )
 parser.add_argument(
-    "-total_ports", help="list total number of nodes in the mesh", action="store_true"
+    "--total_ports", help="list total number of nodes in the mesh", action="store_true"
 )
 parser.add_argument(
-    "-s",
-    "-state",
+    "--state",
     choices=["provisioned", "started", "active", "error", "stopped", "suspended"],
     help="list nodes in your mesh based on their state",
 )
 parser.add_argument(
-    "-ps",
-    "-port_state",
+    "--port_state",
     choices=["open", "closed"],
     help="list nodes in your mesh based on their port state",
 )
 parser.add_argument(
-    "-pn",
     "-port_number",
     type=int,
     help="search for nodes in your mesh based on their port number",
 )
 parser.add_argument(
-    "-c",
-    "-cloud",
+    "--cloud",
     choices=["aws", "azure", "gcp"],
     help="list active nodes by cloud provider, for example -c aws",
 )
 parser.add_argument(
-    "-cat",
-    "-category",
+    "--category",
     choices=["source", "ingest", "enrich", "serve"],
     help="list active nodes by cloud provider, for example -c aws",
 )
 parser.add_argument(
-    "-t",
-    "-type",
+    "--type",
     choices=["digital-twin", "aggregate", "operational"],
     help="list active nodes by node type, for example -t operational",
 )
 parser.add_argument(
-    "-d",
-    "-date",
+    "--date",
     type=int,
     help="list nodes created in the last X days, for example -date 30 to view nodes created in the last 30 days",
 )
 
 parser.add_argument(
-    "-ap", "-add_port", help="add a port to a node.", action="store_true"
+    "--add_port", help="add a port to a node.", action="store_true"
 )
 parser.add_argument(
-    "-an", "-add_node", help="add a new node to the mesh", action="store_true"
+    "--add_node", help="add a new node to the mesh", action="store_true"
 )
 parser.add_argument(
-    "-delete_node", help="delete a node from your mesh"
+    "--delete_node", help="delete a node from your mesh"
 )
 parser.add_argument(
-    "-delete_port", nargs=2, help="delete a port from a node"
+    "--delete_port", nargs=2, help="delete a port from a node"
 )
 parser.add_argument(
-    "-n",
-    "-name",
+    "--name",
     help="search for a node by name.",
 )
 
 parser.add_argument(
-    "-id",
+    "--id",
     help="search for a node by name.",
 )
 parser.add_argument(
-    "-i",
-    "-interface",
+    "--interface",
     help="search for available interfaces on a specific node. Requires you to provide a node id.",
 )
 parser.add_argument(
-    "-launch_node",
+    "--launch_node",
     help="launch a node",
 )
 parser.add_argument(
-    "-view_ancestors",
+    "--view_ancestors",
     help="view the ancestors of a node",
 )
 parser.add_argument(
-    "-view_descendants",
+    "--view_descendants",
     help="view the ancestors of a node",
 )
 parser.add_argument(
-    "-get_schema",
+    "--get_schema",
     help="view the ancestors of a node",
 )
 
 args = parser.parse_args()
 
-if args.l:
+if args.login:
     get_access_token()
 if args.all_nodes:
     list_all_nodes()
 if args.all_ports:
     list_all_ports()
-if args.s:
-    state = str(args.s)
+if args.state:
+    state = str(args.state)
     list_by_state(state)
-if args.c:
-    cloud_provider = str(args.c)
+if args.cloud:
+    cloud_provider = str(args.cloud)
     list_by_cloud_provider(cloud_provider)
-if args.cat:
-    node_category = str(args.cat)
+if args.category:
+    node_category = str(args.category)
     list_by_node_category(node_category)
-if args.t:
-    node_type = str(args.t)
+if args.type:
+    node_type = str(args.type)
     list_by_node_type(node_type)
-if args.d:
-    days_ago = int(args.d)
+if args.date:
+    days_ago = int(args.date)
     list_by_date(days_ago)
-if args.ps:
-    port_state = str(args.ps)
+if args.port_state:
+    port_state = str(args.port_state)
     list_by_port_state(port_state)
-if args.pn:
-    port_number = int(args.pn)
+if args.port_number:
+    port_number = int(args.port_number)
     search_by_port_number(port_number)
-if args.ap:
+if args.add_port:
     add_new_port()
-if args.an:
+if args.add_node:
     add_new_node()
-if args.n:
+if args.name:
     node_name = str(args.n)
     search_by_name(node_name)
 if args.id:
     node_id = str(args.id)
     search_by_id(node_id)
-if args.i:
-    node_id = str(args.i)
+if args.interface:
+    node_id = str(args.interface)
     view_node_interfaces(node_id)
 if args.total_nodes:
     list_total_nodes()
